6b51036adda833774d19efbddf3bf523060a8f64
Stashun <sashmore288@gmail.com>
2018-02-12 10:06:21 -0500

    updates to server.js

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ var path = require("path");
 
 var app = express();
 var PORT= 3000;
-
+console.log(__dirname)
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static('./app'))
+app.use(express.static(path.resolve(__dirname, '/app/public/home.html')))
+app.get('*', (req,res) => {
 
+	var index = path.resolve(__dirname, '/app/public/home.html')
+	res.sendFile(index)
+})
 //require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
